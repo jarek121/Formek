463585f2ba982d5d2fe06a2d5415c4dbc2213e2b
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-02-14 22:51:38 +0100

    zmiana pliku widoku

--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import authenticate, login
+# from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # from .forms import LoginForm
 from .models import AnkietaSkorupy
 from .forms import AnkietaForm
