f5104068cdcd15081de8ccc4168593f9b229d8ee
Jarosław Hyżyk <jaroslawhyzyk@gmail.com>
2026-02-14 16:49:42 +0100

    edycja pliku wievs.py i szablonu, dodanie logowania usera do panelu

--- login/views.py
+++ login/views.py
@@ -3,21 +3,30 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .forms import LoginForm
+#
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username = cd['username'], password = cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Zostałeś zalogowany')
+#                 else: 
+#                     return HttpResponse('Konto zablokowane')
+#             else:
+#                 return HttpResponse('Niepoprawne dane')
+#     else:
+#         form = LoginForm()
+#         return render(request, 'registration/login.html', {'form': form}) 
+# 
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username = cd['username'], password = cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Zostałeś zalogowany')
-                else: 
-                    return HttpResponse('Konto zablokowane')
-            else:
-                return HttpResponse('Niepoprawne dane')
-    else:
-        form = LoginForm()
-        return render(request, 'registration/login.html', {'form': form})       
+#Widok dashboard
+@login_required
+def dashboard(request):
+    return render(request,
+        'registration/dashboard.html', 
+        {'section': 'dashboard'}
+    )
